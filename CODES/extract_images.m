354e7f3e530c7f229f13fd98ba0a97e441c9d11d
Athina Lange <alange@ucsd.edu>
2023-09-11 14:42:18 -0700

    Update extract_images.m

--- CODES/extract_images.m
+++ CODES/extract_images.m
@@ -1,28 +1,49 @@
-%% Extract IMAGES
+%% extract_images
+%
+% - Obtain day relevant data 
+%       - camera intrinsics
+%       - Products
+%       - extraction frame rates
+% - Do flight specific checks
+%       - Pull initial drone position and pose from metadata (using exiftool)
+%       - extract initial frame (using ffmpeg)
+%       - confirm distortion
+%       - confirm inital drone position and pose from gcps
+%       - check products
+%
+%
+% TODO
+%
+%
+%
+% (c) Athina Lange, Coastal Processes Group, Scripps Institution of Oceanography - Sept 2023
 
-for dd = 1:length(data_files)
+%%
+% repeat for each day
+for dd = 1 : length(data_files)
     clearvars -except dd *_dir user_email data_files
-    cd([data_files(dd).folder '/' data_files(dd).name])
+    cd(fullfile(data_files(dd).folder, data_files(dd).name))
 
-    load([data_files(dd).folder '/' data_files(dd).name '/input_data.mat'])
+    load(fullfile(data_files(dd).folder, data_files(dd).name, 'input_data.mat'))
 
-    for ff = 1:length(flights)
-        odir = [flights(ff).folder '/' flights(ff).name];
+    % repeat for each flight
+    for ff = 1 : length(flights)
+        odir = fullfile(flights(ff).folder, flights(ff).name);
         oname = [data_files(dd).name '_' flights(ff).name];
         cd(odir) 
 
-        for hh = 1:length(extract_Hz)
+        % repeat for each extracted frame rate
+        for hh = 1 : length(extract_Hz)
             if ~exist(sprintf('images_%iHz', extract_Hz(hh)), 'dir')
                 mkdir(sprintf('images_%iHz', extract_Hz(hh)))
             end
-            imageDirectory = sprintf('images_%iHz/', extract_Hz(hh));
+            imageDirectory = sprintf('images_%iHz', extract_Hz(hh));
             load(fullfile(odir, 'Processed_data', 'Inital_coordinates'), 'jpg_id', 'mov_id', 'C')
 
-            
-            % Extract images from each video
-            for ii = 1:length(mov_id)
-                mkdir([imageDirectory char(string(ii))])
-                system(['ffmpeg -i ' char(string(C.FileName(mov_id(ii)))) ' -qscale:v 2 -r ' char(string(extract_Hz(hh))) ' ' imageDirectory '' char(string(ii)) '/Frame_%04d.jpg'])
+            % repeat for each video
+            for ii = 1 : length(mov_id)
+                mkdir(fullfile(imageDirectory, char(string(ii))))
+                system(['ffmpeg -i ' char(string(C.FileName(mov_id(ii)))) ' -qscale:v 2 -r ' char(string(extract_Hz(hh))) ' ' fullfile(imageDirectory, char(string(ii)), 'Frame_%04d.jpg')])
             end
             % Combine images and rename into sequential
             for ii = 1:length(mov_id)
@@ -31,7 +52,7 @@ for dd = 1:length(data_files)
                 L = dir([imageDirectory char(string(ii))]); L([L.isdir] == 1) = []; L = string(extractfield(L, 'name')');
 
                 if ii == 1
-                    movefile(sprintf([imageDirectory '%d/Frame_*'], ii), imageDirectory)
+                    movefile(fullfile(imageDirectory, char(string(ii)), 'Frame_*'), imageDirectory)
                 else
                     for ll = 1:length(L)
                         if ll < 10
@@ -44,17 +65,18 @@ for dd = 1:length(data_files)
                             id = [char(string(ll))];
                         end
 
-                        movefile(sprintf([imageDirectory '%d/Frame_%s.jpg'], ii,id),sprintf([imageDirectory 'Frame_%s.jpg'], char(string(ll+Lfull))))
+                        movefile(fullfile(imageDirectory, char(string(ii)), ['Frame_' id '.jpg']), fullfile(imageDirectory, char(string(ii)), ['Frame_' char(string(ll+Lfull)) '.jpg']))
                     end
                 end  % if ii == 1
             end % for ii = 1:length(mov_id)
 
             % remove placeholder folders
-            for ii = 1:length(mov_id); rmdir(sprintf([imageDirectory '%d'], ii), 's'); end
+            for ii = 1:length(mov_id); rmdir(fullfile(imageDirectory, char(string(ii))), 's'); end
 
         end % for hh = 1:length(extract_Hz)
-
-        sendmail(user_email{2}, [oname '- Image Extraction Done'])
-
+    
+        if exist('user_email', 'var')
+            sendmail(user_email{2}, [oname '- Image Extraction Done'])
+        end
     end % for ff = 1:length(flights)
 end % for dd = 1:length(data_files)
