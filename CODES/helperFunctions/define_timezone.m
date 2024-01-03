29223e3bb69b09fda9a03f3230e916d9b6b1b00f
Athina Lange <alange@ucsd.edu>
2024-01-03 14:51:02 -0800

    documentation define_timezone.m

--- CODES/helperFunctions/define_timezone.m
+++ CODES/helperFunctions/define_timezone.m
@@ -1,7 +1,11 @@
 function [TimeZone] = define_timezone
-% Define timezone
-% Returns string of selected timezone. Can be used in datetime construction.
+% TIMEZONE Define timezone
+%     [TimeZone] = define_timezone User prompted timezone selection
 %
+%     TimeZone (string): user selected timezone
+
+% Can be used in datetime construction.
+
     cont_areas = [{'Africa'}, {'America'}, {'Antarctica'}, {'Arctic'}, {'Asia'}, {'Atlantic'}, {'Australia'}, {'Europe'}, {'Indian'}, {'Pacific'}, {'All'}];
     [ind_area,tf] = listdlg('ListString', cont_areas, 'SelectionMode','single', 'InitialValue',1, 'PromptString', {'Which geographic region are you in?'});
     
